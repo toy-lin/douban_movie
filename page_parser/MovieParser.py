19455e4e96bff96962a4006ad4f4626b01919dc7
toy-lin <361700704@qq.com>
2018-11-17 16:43:22 +0800

    add other movie info

--- page_parser/MovieParser.py
+++ page_parser/MovieParser.py
@@ -5,6 +5,7 @@
 
 from bs4 import BeautifulSoup
 from page_parser import Entity
+import re
 
 
 class MovieParser:
@@ -88,7 +89,7 @@ class MovieParser:
     def __get_score(self):
         try:
             info = self.__soup.find('strong', {'property': 'v:average'})
-            self.__movie['score'] = info.text
+            self.__movie['score'] = float(info.text)
         except:
             pass
 
@@ -126,6 +127,59 @@ class MovieParser:
         except:
             pass
 
+    def __get_score_count(self):
+        try:
+            info = self.__soup.find('span', {'property': 'v:votes'})
+            self.__movie['score_count'] = int(info.text)
+        except:
+            pass
+
+    def __get_rate_stars(self):
+        try:
+            infos = self.__soup.findAll('span', {'class': 'rating_per'})
+            rate_keys = ['rate_five_star','rate_four_star','rate_three_star','rate_two_star','rate_one_star']
+            for i_rate in range(5):
+                self.__movie[rate_keys[i_rate]] = float(infos[i_rate].text[:-1])/100
+        except:
+            pass
+
+    def __get_comment_count(self):
+        try:
+            info = self.__soup.find('a',{'href':'reviews'})
+            self.__movie['comment_count'] = int(re.search(r'[0-9]+',str(info.text)).group())
+        except:
+            pass
+    
+    def __get_comment_short_count(self):
+        try:
+            info = self.__soup.find('div',{'id':'comments-section'})
+            result = info.findAll('a')
+            for a in result:
+                if '更多短评' in str(a.text):
+                    self.__movie['comment_short_count'] = int(re.search(r'[0-9]+',str(a.text)).group())
+                    break
+        except:
+            pass
+    
+    def __get_cover_url(self):
+        try:
+            self.__movie['cover_url'] = self.__soup.find('img',{'rel':'v:image'}).attrs['src']
+        except:
+            pass
+
+    def __get_next_movie_ids(self):
+        try:
+            movies_info = self.__soup.find('div',{'class':'recommendations-bd'})
+            urls = movies_info.findAll('a')
+            result = []
+            for url in urls:
+                movie_id = re.search('[0-9]+',url.attrs['href']).group()
+                if movie_id:
+                    result.append(movie_id)
+            self.__movie['next_movie_ids'] = result
+        except:
+            pass
+
     def __get_others(self):
         try:
             info = self.__soup.find('div', id='info')
@@ -137,14 +191,16 @@ class MovieParser:
                     continue
                 if str(info[i]).find('语言') != -1:
                     self.__movie['languages'] = info[i+1].replace(' / ', ',').strip()
-                if str(info[i]).find('制片国家') != -1:
+                elif str(info[i]).find('制片国家') != -1:
                     self.__movie['release_region'] = info[i + 1].replace(' / ', ',').strip()
-                if str(info[i]).find('又名') != -1:
+                elif str(info[i]).find('又名') != -1:
                     self.__movie['alias'] = info[i + 1].replace(' / ', ',').strip()
-                if str(info[i]).find('编剧') != -1:
+                elif str(info[i]).find('编剧') != -1:
                     item = str(info[i])
                     self.__movie['scriptwriters'] = \
                         item[item.find('/">')+3:item.find('</a')]
+                elif str(info[i]).find('www.imdb.com/title') != -1:
+                    self.__movie['imdb_id'] = str(info[i]).split('<')[1].split('>')[1]
                 i += 1
         except:
             pass
@@ -189,7 +245,7 @@ class MovieParser:
         if self.__is_404_page():
             return None
 
-        print(self.__html_doc)
+        #print(self.__html_doc)
 
         self.__set_bs_soup()
 
@@ -201,10 +257,16 @@ class MovieParser:
         self.__get_duration()
         self.__release_date()
         self.__get_score()
+        self.__get_score_count()
         self.__get_tags()
         self.__get_description()
+        self.__get_rate_stars()
+        self.__get_comment_count()
+        self.__get_comment_short_count()
         self.__get_others()
         self.__get_scriptwriters()
+        self.__get_cover_url()
+        self.__get_next_movie_ids()
 
         return self.__movie
 
--- storage/DbHelper.py
+++ storage/DbHelper.py
@@ -25,16 +25,51 @@ class DbHelper:
             db=config['mysql']['db_name'],
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor)
+        with self.__connection.cursor() as cursor:
+            createDb = "CREATE DATABASE IF NOT EXISTS douban;"
+            cursor.execute(createDb)
+            self.__connection.commit()
+        with self.__connection.cursor() as cursor:    
+            createTb = "CREATE TABLE IF NOT EXISTS movie(\
+                id INT NOT NULL AUTO_INCREMENT,\
+                douban_id VARCHAR(10) UNIQUE,\
+                title VARCHAR(100),\
+                directors VARCHAR(20),\
+                scriptwriters VARCHAR(20),\
+                actors VARCHAR(500),\
+                types VARCHAR(50),\
+                release_region VARCHAR(20),\
+                release_date DATE,\
+                alias VARCHAR(100) DEFAULT NULL,\
+                languages VARCHAR(20),\
+                duration VARCHAR(20),\
+                score FLOAT,\
+                score_count INT,\
+                description VARCHAR(2000),\
+                tags VARCHAR(100),\
+                imdb_id VARCHAR(20),\
+                comment_short_count INT,\
+                comment_count INT,\
+                rate_five_start FLOAT,\
+                rate_four_start FLOAT,\
+                rate_three_start FLOAT,\
+                rate_two_start FLOAT,\
+                rate_one_start FLOAT,\
+                cover_url VARCHAR(100),\
+                PRIMARY KEY (id)\
+            )"
+            cursor.execute(createTb)
+            self.__connection.commit()
 
     def insert_movie(self, movie):
         try:
             with self.__connection.cursor() as cursor:
                 sql = "INSERT IGNORE INTO `movie` (`douban_id`, `title`, `directors`, " \
                       "`scriptwriters`, `actors`, `types`,`release_region`," \
-                      "`release_date`,`alias`,`languages`,`duration`,`score`," \
-                      "`description`,`tags`) VALUES (%s," \
-                      "%s, %s, %s, %s, %s, %s, %s," \
-                      "%s, %s, %s, %s, %s, %s);"
+                      "`release_date`,`alias`,`languages`,`duration`,`score`,`score_count`," \
+                      "`description`,`tags`,`imdb_id`,`comment_short_count`,`comment_count`,"\
+                      "`rate_five_start`,`rate_four_start`,`rate_three_start`,`rate_two_start`,`rate_one_start`,`cover_url`)"\
+                      " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
                 cursor.execute(sql, (
                     movie['douban_id'],
                     movie['title'],
@@ -48,12 +83,36 @@ class DbHelper:
                     movie['languages'],
                     movie['duration'],
                     movie['score'],
+                    movie.get('score_count',0),
                     movie['description'],
-                    movie['tags']
+                    movie['tags'],
+                    movie.get('imdb_id',''),
+                    movie['comment_short_count'],
+                    movie['comment_count'],
+                    movie['rate_five_star'],
+                    movie['rate_four_star'],
+                    movie['rate_three_star'],
+                    movie['rate_two_star'],
+                    movie['rate_one_star'],
+                    movie['cover_url']
                 ))
                 self.__connection.commit()
         finally:
             pass
+
+    def is_movie_id_exists(self, id):
+        with self.__connection.cursor() as cursor:
+            cursor.execute('SELECT COUNT(douban_id) FROM movie WHERE douban_id=%s',id)
+            self.__connection.commit()
+            return cursor._rows[0]['COUNT(douban_id)']
+    
+    def get_last_movie_id(self):
+        with self.__connection.cursor() as cursor:
+            result = cursor.execute('SELECT douban_id FROM movie ORDER BY id DESC limit 1')
+            if result <= 0:
+                return None
+            else:
+                return cursor._rows[0]['douban_id']
 
     def close_db(self):
         self.__connection.close()
